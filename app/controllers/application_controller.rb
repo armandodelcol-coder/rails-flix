4df36b19a5e4fe37494451e7ded50c17a0020a9d
Armando <atdc.codemaster@gmail.com>
2020-08-26 20:36:22 -0300

    lesson 20: the flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  # By default, Rails supports setting the :notice and :alert flash types when calling the redirect_to method
+  add_flash_types(:danger)
+
 end
