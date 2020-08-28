8f7fc19a0d7a06ec7d7ad6fa6cee0766f5087fe8
Armando <atdc.codemaster@gmail.com>
2020-08-28 19:40:53 -0300

    lesson 38: many-to-man with checkboxes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+Genre.create!(name: "Action")
+Genre.create!(name: "Comedy")
+Genre.create!(name: "Drama")
+Genre.create!(name: "Romance")
+Genre.create!(name: "Thriller")
+Genre.create!(name: "Fantasy")
+Genre.create!(name: "Documentary")
+Genre.create!(name: "Adventure")
+Genre.create!(name: "Animation")
+Genre.create!(name: "Sci-Fi")
+
+User.create!(name: "Armando", username: "armando", email: "atdc.codemaster@gmail.com", password: "123456", admin: true)
+
 Movie.create!([
   {
     title: 'Avengers: Endgame',
