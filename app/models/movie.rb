26d66746b7d056e06f3fa0a71dd3bd2a9a247010
Armando <atdc.codemaster@gmail.com>
2020-08-29 00:00:50 -0300

    lesson 40: friendly urls and callbacks

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  before_save :set_slug
 
   RATINGS = %w(G PG PG-13 R NC-17)
 
@@ -28,6 +29,10 @@ class Movie < ApplicationRecord
     (self.average_stars / 5.0) * 100
   end
 
+  def to_param
+    slug
+  end
+
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
@@ -35,7 +40,7 @@ class Movie < ApplicationRecord
   has_many :characterizations, dependent: :destroy
   has_many :genres, through: :characterizations
 
-  validates :title, :released_on, :duration, presence: true
+  validates :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
   validates :image_file_name, format: {
@@ -43,5 +48,12 @@ class Movie < ApplicationRecord
     message: "must be a JPG or PNG image"
   }
   validates :rating, inclusion: { in: RATINGS }
+  validates :title, presence: true, uniqueness: true
 
+  private
+
+    def set_slug
+      self.slug = title.parameterize
+    end
 end
+
